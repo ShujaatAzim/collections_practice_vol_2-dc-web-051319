aa25cf7353242b42202e409bd001ff894d3a5022
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-13 21:22:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
-# your code goes here
+def begins_with_r(array)
+
+end
+
+def contain_a(array)
+
+end
+
+def first_wa(array)
+
+end
+
+def remove_non_strings(array)
+
+end
+
+def count_elements(array)
+
+end
+
+def merge_data(enumerable1, enumerable2)
+
+end
+
+def find_cool(hash)
+
+end
+
+def organize_schools(hash_of_schools)
+
+end
